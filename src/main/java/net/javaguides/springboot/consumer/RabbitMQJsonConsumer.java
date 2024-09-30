00f1daf7da2e848b318c2f3291199adf171937a0
batuhansicakyuz <batuhansicakyuz@gmail.com>
2024-09-30 13:50:04 +0300

    rabbitMQ springboot project addedd

--- src/main/java/net/javaguides/springboot/consumer/RabbitMQJsonConsumer.java
+++ src/main/java/net/javaguides/springboot/consumer/RabbitMQJsonConsumer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitMQJsonConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonConsumer.class);
-
+//
     @RabbitListener(queues = {"${rabbitmq.queue.json.name}"})
     public void consumeJsonMessage(User user){
         LOGGER.info(String.format("Received JSON message -> %s", user.toString()));
